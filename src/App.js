c2776839100354f6bff2033ec2c571c9f1601120
Alex Artemov <artemov.sandro@gmail.com>
2023-10-30 10:39:08 -0400

    dogs react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Dog from './components/Dog';
+
+const dogs = [
+  {name: 'Ryder', age: 4, breed: 'PR Street Dog' },
+  {name: 'Goldy', age: 12, breed: 'Yellow Lab' },
+  {name: 'Bailey', age: 4, breed: 'PR Street Dog' },
+  
+  
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dogs:</h1>
+        {dogs.map(dog => <Dog age={dog.age} name={dog.name} breed={dog.breed} /> )}
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
